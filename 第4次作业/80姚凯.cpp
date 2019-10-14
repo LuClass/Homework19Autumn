7f718032c4aedec51b8c7a49ec51118c631f99b9
919106840542 <55878492+919106840542@users.noreply.github.com>
2019-10-14 10:52:23 +0800

    姚凯第四次作业

--- 第4次作业/80姚凯.cpp
+++ 第4次作业/80姚凯.cpp
@@ -1,4 +1,4 @@
-
+68.(4)
 #include <iostream>
 using namespace std;
 int main (void)
@@ -13,5 +13,44 @@ int main (void)
 	cout<<"count="<<count<<endl;
 	return 0;
 }
- 
- 
+
+(7)
+#include <iostream>
+using namespace std;
+int main (void)
+{
+	int count=2,a=3,b=2;
+	cout<<"1/2"<<endl;
+	cout<<"3/2"<<endl;
+	for(;count<=20;count++)
+	{
+		a=a+b;
+		b=a-b;
+		cout<<a<<'/'<<b<<endl;
+    }
+	return 0;
+}
+
+(8)
+#include <iostream>
+using namespace std;
+int main (void)
+{
+	int x;
+	cin>>x;
+	if(x>0)
+	for (int i=1;i<=x;i++)
+	{if(x%i==0)
+	cout<<i<<" ";
+    }
+    cout<<endl;
+	for (int a=2;a<=100;a++)
+	 {int sum=0;
+	   for (int m=1;m<a;m++)
+	   if(a%m==0)
+	   sum+=m;
+	   if(sum==a)
+	   cout<<a<<endl;
+	 }
+	 	return 0;
+}
