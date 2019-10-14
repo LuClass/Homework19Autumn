cd3516fff13e9ef90e885c1fb00c0ec245d4610c
919106840542 <55878492+919106840542@users.noreply.github.com>
2019-10-14 10:45:27 +0800

    姚凯第四次作业

---
+++ 第4次作业/80姚凯.cpp
@@ -0,0 +1,17 @@
+
+#include <iostream>
+using namespace std;
+int main (void)
+{
+	int count=0;
+	char ch=0;
+	do{
+		ch=cin.get( );
+		if(ch>='0'&&ch<='9')
+		count++;
+	}while (ch!='\n');
+	cout<<"count="<<count<<endl;
+	return 0;
+}
+ 
+ 
