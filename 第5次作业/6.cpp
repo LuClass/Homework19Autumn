2ab34b823d6038e1fe0049c42303de53798881b8
djejdjejf <41856990+djejdjejf@users.noreply.github.com>
2019-10-18 09:15:45 +0800

    刘睿捷的第五次作业

---
+++ 第5次作业/6.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(void)
+{
+	
+	cout<<"   ** \n";
+	cout<<" **  **\n";
+	cout<<"**    *\n";
+	cout<<"*     \n";
+	cout<<"*     \n";
+	cout<<"**    *\n";
+	cout<<" **  **\n";
+	cout<<"   ** \n";
+	return 0;
+}
