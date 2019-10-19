e18ad701a3b736bd2ce914c34200bc569ec206d4
Alpha-kc <56211928+Alpha-kc@users.noreply.github.com>
2019-10-19 20:10:25 +0800

    41-赵凯辰第五次作业

---
+++ 第5次作业/10.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+void main()
+{    
+     cout<<'b'<<'b'<<'\n'; 
+     for(int i=1;i<11;i++)
+	 {
+         for(int j=1;j<=i;j++)    
+		     cout<<'a';
+         cout<<'\n';
+	 }
+}
