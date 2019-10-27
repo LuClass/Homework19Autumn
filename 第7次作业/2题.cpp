4c4195114931bd6b5e8b585c81e64b0573d9c0bf
917103860135 <55949715+917103860135@users.noreply.github.com>
2019-10-27 22:10:49 +0800

    917103860135_温宇轩

---
+++ 第7次作业/2题.cpp
@@ -0,0 +1,44 @@
+
+
+
+#include<iostream>
+using namespace std;
+main(){
+	long int i;
+	int bonus1,bonus2,bonus4,bonus6,bonus10,bonus;
+	cout<<"ÇëÊäÈëÀûÈó"<<endl; 
+	cin>>i;
+    bonus1=100000*0.1;
+    bonus2=bonus1+100000*0.075;
+    bonus4=bonus2+200000*0.05;
+    bonus6=bonus4+200000*0.03;
+    bonus10=bonus6+400000*0.015;
+    switch(int(i/100000))
+  {
+  	case 0:bonus=i*0.1;
+  	       break;
+  	case 1:bonus=bonus1+(i-100000)*0.075;
+  	       break;
+    case 2:bonus=bonus2+(i-200000)*0.05;
+           break;
+    case 3:bonus=bonus2+(i-200000)*0.05;
+           break;
+    case 4:bonus=bonus4+(i-400000)*0.03;
+           break;
+    case 5:bonus=bonus4+(i-400000)*0.03;
+           break;
+    case 6:bonus=bonus6+(i-600000)*0.015;
+           break;
+    case 7:bonus=bonus6+(i-600000)*0.015;
+           break;
+    case 8:bonus=bonus6+(i-600000)*0.015;
+           break;
+    case 9:bonus=bonus6+(i-600000)*0.015;
+           break;
+    default:bonus=bonus10+(i-1000000)*0.01;
+    	cout<<bonus<<endl;
+  }
+	}
+
+
+ 
