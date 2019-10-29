22c006f721cb30ca3e3650e01050d04e60069844
agoooo <51218324+agoooo@users.noreply.github.com>
2019-10-29 19:27:57 +0800

    戴值洋第八次作业

---
+++ 第8次作业/917000720227戴值洋21(6).cpp
@@ -0,0 +1,21 @@
+using namespace std;
+#include <iostream>
+int GCD(int m,int n)
+{
+	if(m%n==0)
+    return n;
+    else
+    return GCD(n,m%n);	 
+}
+int LCM(int m,int n)
+{
+	int gcd=GCD(m,n);
+	return m*n/gcd;
+}
+int main(){	
+	cout<<"请输入两个正整数：";
+	int m,n;
+	cin>>m>>n;
+	cout<<"最大公约数为："<<GCD(m,n)<<" 最小公倍数为："<<LCM(m,n); 
+	return 0;
+}
