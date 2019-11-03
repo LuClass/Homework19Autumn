20959f15c98fbb1c93a10ce3040491a4ed8ce5d9
zhangjcNJUST <zhangjiacheng@njust.edu.cn>
2019-11-03 21:03:19 +0800

    change file name 戴值洋

--- 第9次作业/hex.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-using namespace std;
-#include <iomanip>
-void toHex(int x)
-{
-	cout<<hex<<x;
-}
--- 第9次作业/max.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-using namespace std;
-int max(int a,int b)
-{
-	return a>b?a:b;
-}
