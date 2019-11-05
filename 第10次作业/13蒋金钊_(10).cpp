276a171901da24915f45431fce8af0071a066d34
Linapz <51466645+Linapz@users.noreply.github.com>
2019-11-05 17:20:30 +0800

    蒋金钊第10次作业

---
+++ 第10次作业/13蒋金钊_(10).cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+#include<string.h>
+
+int getNum(int a[]) {
+	int k = 0;
+	for (int i = 1; i < 22; i++) {
+		if (a[i] == 0) {
+			k++;
+		}
+	}
+	return k;
+}
+
+int main() {
+	int a[22];
+	memset(a, 0 , sizeof(a));
+	int k = 1;
+	while (getNum(a) != 1) {
+		for (int i = 1; i < 22; i++) {
+			if (a[i] == 0) {
+				k++;
+			}
+			if (k == 5) {
+				a[i] = 1;
+				k = 1;
+			}
+		}
+	}
+	for (int i = 1; i < 22; i++) {
+		if (a[i] == 0) {
+			cout << i;
+		}
+	}
+	return 0;
+}
