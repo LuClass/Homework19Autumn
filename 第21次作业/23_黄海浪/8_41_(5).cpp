c093f6fa72222fb161ed6799f6e61afe951e110b
9181040G0818 <55886774+9181040G0818@users.noreply.github.com>
2019-12-10 20:15:43 +0800

    Create 8_41_(5).cpp

---
+++ 第21次作业/23_黄海浪/8_41_(5).cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+/*
+ *黄海浪 9181040G0818
+ *2019.12.10
+ *第 21 次作业
+ */
+using namespace std;
+
+int main() {
+
+	string str;
+	getline(cin,str);
+	unsigned long len = str.length();
+	int *b = new int [len/2+1];
+	int tmp = 0,index=0;
+	bool isget = false;
+	for(int i=0; i<len; ++i) {
+		if(str[i]>='0'&&str[i]<='9') {
+			tmp = tmp*10 + str[i]-'0';
+			isget = true;
+			if(i!=len-1) continue;
+		}
+		if(isget) {
+			b[index++] = tmp;
+			tmp = 0;
+			isget = false;
+		}
+	}
+	cout<<index<<endl;
+	for(int i=0; i<index; ++i) {
+		cout<<b[i]<<" ";
+	}
+
+	return 0;
+}
