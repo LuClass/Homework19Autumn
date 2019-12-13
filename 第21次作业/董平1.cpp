7a1d96d0e084202bce78a0c28929896527f99563
dplink <52562888+dplink@users.noreply.github.com>
2019-12-13 10:06:11 +0800

    董平第21次作业

---
+++ 第21次作业/董平1.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+char a[10001];
+int main(){
+    //freopen("test.in","r",stdin);
+    //freopen("test.out","w",stdout);
+    while(~scanf("%s",a)){
+        cout<<a;
+    }
+    return 0;
+}
+
