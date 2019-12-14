579607babeccee17d1ee65ea24c78438e3a70a4a
917000720125 <54499539+917000720125@users.noreply.github.com>
2019-12-14 13:18:21 +0800

    陈智源第二十一次作业

---
+++ 第21次作业/41（5）.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	char str[100],*p=str;
+    int i,k=0,key=0,sum,a[100]={0};
+	cin.getline(str,101);
+	for(i=0;*(p+i)!='\0';i++)
+	{ 
+		if((*(p+i)>='0')&&(*(p+i)<='9'))
+		{
+	    	sum=k+1;
+			a[k]=a[k]*10+*(p+i)-'0';
+			key=1;
+		}
+		else if(key)
+		{
+   			a[++k]=0;
+			key=0;
+		}
+	}
+	for(i=0;i<sum;i++)
+	cout << a[i]<<endl;
+	cout << sum<<endl;	
+	return 0;	
+}
